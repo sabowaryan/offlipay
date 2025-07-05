e28228a36362f166a8427e878c7ed603a53271f1
sabowaryan <screentapinc@gmail.com>
2025-07-05 15:56:32 +0100

    mise ç jour generale

---
+++ utils/colors.ts
@@ -0,0 +1,13 @@
+// Palette de couleurs de l'application
+// Inspirée de l'image fournie
+
+export const COLORS = {
+  WARM_RED: '#FF4E45',
+  OUTER_SPACE: '#232945',
+  PERFECT_WHITE: '#FFFFFF',
+};
+
+// Pour chaque couleur :
+// WARM_RED      : RGB(255, 78, 69)   | HEX #FF4E45
+// OUTER_SPACE   : RGB(35, 41, 69)    | HEX #232945
+// PERFECT_WHITE : RGB(255, 255, 255) | HEX #FFFFFF 
