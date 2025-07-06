e036a2b922460876f42abbff6beb42f5177dd592
sabowaryan <screentapinc@gmail.com>
2025-07-07 00:33:44 +0100

    mise

---
+++ app.config.js
@@ -0,0 +1,74 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) return 'com.offlipay.app.dev';
+  if (IS_PREVIEW) return 'com.offlipay.app.preview';
+  return 'com.offlipay.app';
+};
+
+const getAppName = () => {
+  if (IS_DEV) return 'OffliPay (Dev)';
+  if (IS_PREVIEW) return 'OffliPay (Preview)';
+  return 'OffliPay';
+};
+
+export default ({ config }) => ({
+  ...config,
+  name: getAppName(),
+  slug: 'offlipay',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/AppIcons/1024.png',
+  scheme: 'myapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    ...config.ios,
+    supportsTablet: true,
+    bundleIdentifier: getUniqueIdentifier(),
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false,
+    },
+    icon: './assets/images/AppIcons/Assets.xcassets/AppIcon.appiconset/1024.png',
+  },
+  android: {
+    ...config.android,
+    package: getUniqueIdentifier(),
+    adaptiveIcon: {
+      foregroundImage: './assets/images/AppIcons/android/mipmap-xxxhdpi/ic_launcher.png',
+      backgroundColor: '#000000',
+    },
+  },
+  web: {
+    bundler: 'metro',
+    output: 'single',
+    favicon: './assets/images/AppIcons/Assets.xcassets/AppIcon.appiconset/1024.png',
+  },
+  plugins: [
+    'expo-router',
+    'expo-font',
+    'expo-web-browser',
+    [
+      '@sentry/react-native/expo',
+      {
+        url: 'https://sentry.io/',
+        project: 'offlipay',
+        organization: 'stelm-technologies',
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    'expo-router': {
+      appRoot: 'app',
+    },
+    router: {},
+    eas: {
+      projectId: 'd7a567cd-c15d-4ee3-aa03-81fb41fb21a9',
+    },
+  },
+  owner: 'albatros01',
+});
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     'babel-preset-expo',
-    '@react-native/babel-preset'
+    
   ],
   plugins: [
     'react-native-reanimated/plugin',
